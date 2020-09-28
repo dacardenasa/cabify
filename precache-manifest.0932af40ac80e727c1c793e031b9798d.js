4da40fd2a3aebb7ee924f4afa53503bc4ba1fa21
dacardenasa <diego.cardenasaleg@gmail.com>
2020-09-28 11:28:14 -0500

    Updates

---
+++ precache-manifest.0932af40ac80e727c1c793e031b9798d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e4c5a897c327612ea1b53edef13996e",
+    "url": "/cabify/index.html"
+  },
+  {
+    "revision": "a631bd006909e4b45bbb",
+    "url": "/cabify/static/css/main.1bb72a62.chunk.css"
+  },
+  {
+    "revision": "0ee99c07fb2b9807a96c",
+    "url": "/cabify/static/js/2.11b13a7a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cabify/static/js/2.11b13a7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a631bd006909e4b45bbb",
+    "url": "/cabify/static/js/main.db92aa49.chunk.js"
+  },
+  {
+    "revision": "7a063679e76083e7a193",
+    "url": "/cabify/static/js/runtime-main.eae5c1d0.js"
+  },
+  {
+    "revision": "05c4af2fd2ec5db881a44b753b2f7008",
+    "url": "/cabify/static/media/bg-card.05c4af2f.png"
+  }
+]);
